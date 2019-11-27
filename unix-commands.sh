080649b0fee9d92e625617a83f96f6b83c604645
vipertoothlabs <47669479+vipertoothlabs@users.noreply.github.com>
2019-11-27 17:55:38 -0600

    Update unix-commands.sh

--- unix-commands.sh
+++ unix-commands.sh
@@ -1,1 +1,5 @@
 docker run -dit --net=host -e ENABLE_SSL=1 -e ADV_HOST=192.168.1.3 --name kafka-sandbox lensesio/fast-data-dev:latest
+
+
+export HOST_IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | head -1 | cut -d\  -f2)
+
